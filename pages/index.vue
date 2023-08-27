245c758fd2614b210f84d9d3a709b810643c2c8a
Jeonghun HAN <jasmin@o.shinhan.ac.kr>
2023-08-27 15:57:48 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>This is the home page.</p>
+    <my-component />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import MyComponent from '~/components/MyComponent.vue';
+
+@Component({
+  components: {
+    MyComponent,
+  },
+})
+export default class IndexPage extends Vue {
+  title: string = 'Welcome to My SPA Nuxt App!';
+}
+</script>
+
